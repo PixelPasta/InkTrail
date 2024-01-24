33115cf5e16b64d6472590b2f82942cae217ff70
Pixel Pasta <80410384+PixelPasta@users.noreply.github.com>
2024-01-24 11:28:09 +0530

    I forgor

---
+++ index.js
@@ -0,0 +1,13 @@
+const fetch = require("node-fetch");
+async function get() {
+ let response = await fetch("https://api.jikan.moe/v4/manga?genres=22&page=1&min_score=8")
+ response = await response.json()
+ let lastPage = response.pagination.last_visible_page
+ response = await fetch(`https://api.jikan.moe/v4/manga?genres=22&page=${Math.floor(Math.random() * lastPage+1)}&min_score=8`)
+ response = await response.json()
+ let manga = response.data[Math.floor(Math.random() * response.data.length)]
+ let info = {
+ malID = 
+ }
+}
+get()
