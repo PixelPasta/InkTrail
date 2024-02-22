b84cc9c4ac3f72b80be43a2f41144fafc4836db3
Pixel Pasta <80410384+PixelPasta@users.noreply.github.com>
2024-02-22 11:54:12 +0530

    The most horrifying code you'll ever see

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 const fetch = require("node-fetch");
-async function get() {
- let response = await fetch("https://api.jikan.moe/v4/manga?genres=22&page=1&min_score=8")
- response = await response.json()
- let lastPage = response.pagination.last_visible_page
- response = await fetch(`https://api.jikan.moe/v4/manga?genres=22&page=${Math.floor(Math.random() * lastPage+1)}&min_score=8`)
- response = await response.json()
- let manga = response.data[Math.floor(Math.random() * response.data.length)]
- let info = {
- malURL: manga.url,
- 
- }
-}
-get()
+
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res) => {
+    res.render('index')
+})
+
+app.get('/romance', async (req, res) => {
+    
+    async function get() {
+        let response = await fetch("https://api.jikan.moe/v4/manga?genres=22&page=1&min_score=5&start_date=2018-01-01")
+        response = await response.json()
+        let lastPage = response.pagination.last_visible_page
+        response = await fetch(`https://api.jikan.moe/v4/manga?genres=22&page=${Math.floor(Math.random() * lastPage+1)}&min_score=5&start_date=2018-01-01`)
+        response = await response.json()
+        let manga = response.data[Math.floor(Math.random() * response.data.length)]
+       
+        let info = {
+           mal_url: manga.url,
+           cover: manga.images.jpg.image_url,
+           title_en: manga.title,
+           title_jp: manga.title_japanese,
+           chapter_count: manga.chapters,
+           score: `${manga.score}/10`,
+           desc: manga.synopsis,
+           author: manga.authors[0].name
+        }
+        return info
+       }
+       res.render('romance', await get())
+})
+
+app.get('/public/:id', async (req, res) => {
+    res.sendFile(`${__dirname}/public/${req.params.id}`)
+})
+
+app.listen(port, async () => {
+    console.log(`Listening on ${port}`)
+})
+
+
+
+
+
---
+++ public/gsap.js
@@ -0,0 +1,24 @@
+function getOffset(el) {
+    console.log(el)
+    const rect = el.getBoundingClientRect();
+    return {
+      left: rect.left + window.scrollX,
+      top: rect.top + window.scrollY
+    };
+  }
+
+  const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+
+document.addEventListener('DOMContentLoaded',async function() {
+    let tl = gsap.timeline()
+    tl.from(".Heading", 
+    { opacity: 0, 
+        y: 100, 
+        duration: 1,
+    ease: "bounce.out" });
+await sleep(2000)
+    document.getElementsByClassName("loading")[0].style.display = 'none'
+   document.getElementsByClassName('MainManga')[0].style.display = 'block'
+  
+});
